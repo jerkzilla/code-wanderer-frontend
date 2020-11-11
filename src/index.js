39a5dd1ee394a8ef1fb37184dabd5f8623bec0ad
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-11-11 08:01:01 -0600

    created store, set up redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux'
+import categoryReducer from './reducers/categoryReducer'
+
 import App from './App';
 
-ReactDOM.render(
+let reducer
 
-    <App />,
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(categoryReducer, composeEnhancers(applyMiddleware(thunk)))
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   
   document.getElementById('root') 
 );
