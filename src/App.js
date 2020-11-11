ff6c26f9909c65832ccac451432386ffb67944e2
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-11-11 07:28:27 -0600

    testing connections to backend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 class App extends React.Component {
+  
+  componentDidMount() {
+    fetch('http://localhost:3000/categories')
+    .then(resp => resp.json())
+    .then(data => console.log(data[0].resources))
+  }
+  
   render() {
     return (
       <div className="App">
