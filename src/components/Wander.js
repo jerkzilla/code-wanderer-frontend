ee477605d1c80a3f634703403eda010a7d00a71d
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-12-11 08:28:48 -0600

    update wander redirect

--- src/components/Wander.js
+++ src/components/Wander.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
- class Wander extends Component {
-    
-    render() {
-        return (
-            <div>
-                {window.open(`${this.props.wanders.link}`)}
-                {window.location.assign("https://jerkzilla.github.io/code-wanderer-frontend/")}
-            </div>
-        )
-    }
+class Wander extends Component {
+  render() {
+    return (
+      <div>
+        {window.open(`${this.props.wanders.link}`)}
+        {window.location.assign(
+          "https://johnjamesmarshalljr.github.io/code-wanderer-frontend/"
+        )}
+      </div>
+    );
+  }
 }
 
-const mSTP = globalState => {
-    return {wanders: globalState.wanders}
-}
+const mSTP = (globalState) => {
+  return { wanders: globalState.wanders };
+};
 
-export default connect(mSTP)(Wander)
+export default connect(mSTP)(Wander);
